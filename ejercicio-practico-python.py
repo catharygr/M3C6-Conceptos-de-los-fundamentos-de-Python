c5f98fa2edef297d624dd3a0ac3fafe1f93b51d8
catharygr <103951447+catharygr@users.noreply.github.com>
2026-03-10 14:19:03 +0100

    Realizado el elercicio de Python

--- ejercicio-practico-python.py
+++ ejercicio-practico-python.py
@@ -1,4 +1,6 @@
 # Cree una clase de Python llamada Usuario que use el método init y cree un nombre de usuario y una contraseña. Crea un objeto usando la clase.
+
+
 class Usuario:
   def __init__(self, nombre, contraseña):
     self.nombre = nombre
